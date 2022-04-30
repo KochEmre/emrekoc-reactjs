bf1da6aa5a0f69902715094efb3ba4301b1ab2dc
KochEmre <kochhhemre@gmail.com>
2022-04-30 17:33:09 +0300

    added prettier and prettier-tailwind for dev depencies

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div className="flex-row m-5 bg-white rounded-lg justify-between">
+    <div className="m-5 flex-row justify-between rounded-lg bg-white">
       Home
       {/* <Link to="/">HOME</Link>
       <Link to="/Register">ABOUT</Link> */}
